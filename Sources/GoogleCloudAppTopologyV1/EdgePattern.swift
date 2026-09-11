db36f73c65a6385261a92d93216183c921702436
Carlos O'Ryan <coryan@google.com>
2026-09-11 20:11:51 +0000

    feat: update librarian with serialize-enums-as-strings (#829)
    Update librarian to use strings when serializing enums. Fixed a number
    of unit / integration tests.

--- Sources/GoogleCloudAppTopologyV1/EdgePattern.swift
+++ Sources/GoogleCloudAppTopologyV1/EdgePattern.swift
@@ -151,10 +151,10 @@ public struct EdgePattern: Codable, Equatable, GoogleCloudWKT._AnyPackable,
     public func encode(to encoder: Encoder) throws {
       var container = encoder.singleValueContainer()
       switch self {
-      case .unspecified: return try container.encode(0)
-      case .any: return try container.encode(1)
-      case .to: return try container.encode(2)
-      case .from: return try container.encode(3)
+      case .unspecified: return try container.encode("DIRECTION_UNSPECIFIED")
+      case .any: return try container.encode("ANY")
+      case .to: return try container.encode("TO")
+      case .from: return try container.encode("FROM")
       case .unknownIntValue(let v): return try container.encode(v)
       case .unknownStringValue(let v): return try container.encode(v)
       }
